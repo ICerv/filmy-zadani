3caa006a27c9b11f6ba79248e456832d9e38bd83
Inna Červenková <inna.cervenkova@gmail.com>
2022-10-25 02:21:23 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 console.log('funguju!');
+/*zadani 1.
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data)
+  });
+*/
+const renderMovies = (movies) => {
+  const movieListElm = document.querySelector('.movie-list');
+  movieListElm.innerHTML = movies
+    .map((movie) => {
+      return ` 
+  <li class="movie-detail">
+    <div class="movie-poster">
+      <img 
+        src="${movie.posterUrl}"
+        alt="${movie.title}"
+      />
+    </div>
+    <div class="movie-info">
+      <h2 class="movie-title">${movie.title}</h2>
+      <div class="movie-year">Rok vydání: ${movie.year}</div>
+      <div class="movie-link">
+        <a href="${movie.url}" target="_blank">Odkaz na CSFD</a>
+      </div>
+    </div>
+  </li>
+  `;
+    })
+    .join('');
+};
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => {
+    return response.json();
+  })
+  .then(renderMovies);
